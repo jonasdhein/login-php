68b41106833ac8c76f3580346362b469a776f617
Jonas <jdhein@univates.br>
2022-11-08 22:23:16 -0300

    Comunicação com o banco de dados

---
+++ conexao.php
@@ -0,0 +1,6 @@
+<?php
+
+$conexao = require('./config.php');
+
+//pegar os dados que vieram no formulário
+//e testar se são válidos
---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form method="post" action="conexao.php">
+        <div>
+            <label>Usuário</label>
+            <input type="text" id="usuario" name="usuario">
+        </div>
+        <div>
+            <label>Senha</label>
+            <input type="password" id="senha" name="senha">
+        </div>
+        <div>
+            <input type="submit" id="logar" value="ENTRAR">
+        </div>
+    </form>
+</body>
+
+</html>
