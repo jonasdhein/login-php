6bbb3f96404abd68345539ef1adf2e620e6aeaee
Jonas <jdhein@univates.br>
2022-11-22 21:42:28 -0300

    melhorias na estrutura

--- conexao.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$conexao = require('./config.php');
-
-//pegar os dados que vieram no formulário
-//e testar se são válidos
--- login.php
+++ login.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,22 +10,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+
+    <?php include('components/js.php') ?>
 </head>
 
 <body>
-    <form method="post" action="conexao.php">
-        <div>
-            <label>Usuário</label>
-            <input type="text" id="usuario" name="usuario">
-        </div>
-        <div>
-            <label>Senha</label>
-            <input type="password" id="senha" name="senha">
-        </div>
-        <div>
-            <input type="submit" id="logar" value="ENTRAR">
-        </div>
-    </form>
+    <div class="container">
+        <form method="post" action="actions/actions_login.php">
+            <div>
+                <label>Usuário</label>
+                <input type="text" id="usuario" name="usuario">
+            </div>
+            <div>
+                <label>Senha</label>
+                <input type="password" id="senha" name="senha">
+            </div>
+            <?php
+            if (isset($_SESSION["erro"])) {
+                echo "<script>mensagem_erro('" . $_SESSION['erro'] . "')</script>";
+                unset($_SESSION['erro']);
+            }
+            ?>
+            <div>
+                <input type="submit" id="logar" value="ENTRAR">
+            </div>
+        </form>
+    </div>
+
 </body>
 
 </html>
