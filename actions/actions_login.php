216408c96dd3d9572856ec89c2f970da3908bca8
Jonas <jdhein@univates.br>
2022-11-22 22:01:31 -0300

    sessão completa

--- actions/actions_login.php
+++ actions/actions_login.php
@@ -6,7 +6,7 @@ $conexao = require('../database/config.php');
 $usuario = $_POST["usuario"]; //name do formulário da requisição
 $senha = $_POST["senha"]; //name do formulário da requisição
 
-$sql = " SELECT * FROM usuarios 
+$sql = " SELECT nome FROM usuarios 
         WHERE login = :login 
         AND senha = md5(:senha) ";
 
@@ -17,8 +17,9 @@ $stmt->bindValue(":senha", $senha);
 $stmt->execute(); //executa o SQL com os parametros passados acima
 $retorno = $stmt->fetch(PDO::FETCH_ASSOC); //armazena na variável retorno, os dados obtidos da consulta
 if($retorno){ //retorno está preenchido e não é falso?
-    echo "Olá " . $retorno["nome"];
-    $_SESSION["erro"] = "";
+    $_SESSION["logado"] = true;
+    $_SESSION["usuario"] = $retorno["nome"];
+    header('Location: ../index.php');
 }else{
     $_SESSION["erro"] = "Dados de acesso inválidos";
     header('Location: ../login.php'); //navega até a tela de login
