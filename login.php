ada2b5cbf8ea1747818a80b40876e6bfbbaedaff
Jonas <jdhein@univates.br>
2022-11-22 21:42:55 -0300

    otimização do login.php

--- login.php
+++ login.php
@@ -6,9 +6,6 @@ session_start();
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 
     <?php include('components/js.php') ?>
